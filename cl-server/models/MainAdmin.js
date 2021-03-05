0d40e983182d46d52b218aaa44fce6edd52a7b29
Osiris <potterosiris@gmail.com>
2021-03-05 15:08:56 -0500

    maina schema

--- cl-server/models/MainAdmin.js
+++ cl-server/models/MainAdmin.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const MainAdminSchema = new Schema({
+    name: { type: String, required: true },
+    password: { type: String, required: true }
+})
