8fe75286498b9a17e490cb43d719d179e7930395
Osiris <potterosiris@gmail.com>
2021-03-05 14:35:05 -0500

    mw

--- cl-server/app.js
+++ cl-server/app.js
@@ -4,3 +4,14 @@ const bodyParser = require("body-parser")
 const app = express()
 
 app.use(bodyParser.json())
+
+
+
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || "an unknown error occured" });
+});
