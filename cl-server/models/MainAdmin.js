a5f92dfceeda3b4f23bedcaf4a32c7cf3611cfcf
Osiris <potterosiris@gmail.com>
2021-03-05 15:10:16 -0500

    export

--- cl-server/models/MainAdmin.js
+++ cl-server/models/MainAdmin.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
-const MainAdminSchema = new Schema({
+const mainAdminSchema = new Schema({
     name: { type: String, required: true },
     password: { type: String, required: true }
 })
+
+
+module.exports = mongoose.model("MainAdmin", mainAdminSchema)
