7ddc3be13faa520528b15017cf25d0d0dd10307d
Osiris <potterosiris@gmail.com>
2021-03-05 15:20:55 -0500

    login

--- cl-server/controllers/MainAdmin-controller.js
+++ cl-server/controllers/MainAdmin-controller.js
@@ -1,1 +1,7 @@
 const HttpError = require("../models/HttpError")
+
+const MainAdmin = require("../models/MainAdmin")
+
+const login = async (req, res, next) => {
+    const { name, password } = req.body
+}
