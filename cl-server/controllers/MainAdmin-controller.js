f46ac8651ca39d5cecc07f11c3c025a43f0e7fef
Osiris <potterosiris@gmail.com>
2021-03-06 16:20:56 -0500

    login

--- cl-server/controllers/MainAdmin-controller.js
+++ cl-server/controllers/MainAdmin-controller.js
@@ -31,6 +31,24 @@ const login = async (req, res, next) => {
 
     const { name, password } = req.body
 
+    let existingUser
+    try {
+        existingUser = await MainAdmin.findOne({ name: name })
+    } catch (err) {
+        const error = new HttpError("couldnt find email in our database", 500)
+    }
+    if (!existingUser || existingUser.password !== password) {
+        const error = new HttpError(
+            "wrong stuff",
+            401
+        )
+        return next(error)
+    }
+
+    res.json({
+        message: "logged in",
+        user: existingUser.toObject({ getters: true })
+    })
 
 
 }
