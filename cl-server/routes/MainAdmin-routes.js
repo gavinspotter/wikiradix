5f519e2e8fee9f6040cbe01dc006fce1aec29db7
Osiris <potterosiris@gmail.com>
2021-03-06 16:26:48 -0500

    router

--- cl-server/routes/MainAdmin-routes.js
+++ cl-server/routes/MainAdmin-routes.js
@@ -6,6 +6,6 @@ const router = express.Router()
 
 router.post("/createadmin", mainadminController.createAdmin)
 
-router.post("/adminlogin", mainadminController.login)
+router.post("/adminlogin", mainadminController.adminlogin)
 
 module.exports = router
