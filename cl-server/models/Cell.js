4a8bb56ea4526e79da8f7c1231095781dfb9baad
Osiris <potterosiris@gmail.com>
2021-03-08 18:16:27 -0500

    cell schema

--- cl-server/models/Cell.js
+++ cl-server/models/Cell.js
@@ -3,3 +3,18 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
+const cellSchema = Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    image: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+    organipar: [{}],
+    organelle: [{}]
+
+})
