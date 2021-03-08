eff54177205731b5916b3e55b7ec23817b6689ca
Osiris <potterosiris@gmail.com>
2021-03-08 17:28:55 -0500

    description

--- cl-server/models/System.js
+++ cl-server/models/System.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 
 const systemSchema = Schema({
     systemName: { type: String, required: true },
+    description: { type: String }
     image: { type: String },
     organs: [{}]
 })
