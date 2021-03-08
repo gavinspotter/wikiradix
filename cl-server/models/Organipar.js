7e92787a5501a90a604b7e4e1b6be47a38a15888
Osiris <potterosiris@gmail.com>
2021-03-08 18:22:14 -0500

    schema

--- cl-server/models/Organipar.js
+++ cl-server/models/Organipar.js
@@ -2,3 +2,19 @@ const mongoose = require("mongoose")
 
 
 const Schema = mongoose.Schema
+
+const organiparSchema = Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    image: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+    organ: {},
+    cell: [{}]
+})
+
