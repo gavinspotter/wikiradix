1729b0dfa0507cfe4cfcf9c9b5a40c6ef1bf4465
Osiris <potterosiris@gmail.com>
2021-03-08 18:31:11 -0500

    schema

--- cl-server/models/Celluel.js
+++ cl-server/models/Celluel.js
@@ -2,3 +2,17 @@ const mongoose = require("mongoose")
 
 
 const Schema = mongoose.Schema
+
+const celluelSchema = Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    image: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+    organelle: { type: String }
+})
--- cl-server/models/Organelle.js
+++ cl-server/models/Organelle.js
@@ -2,3 +2,18 @@ const mongoose = require("mongoose")
 
 
 const Schema = mongoose.Schema
+
+const organelleSchema = Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    image: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+    cell: {},
+    celluel: [{}]
+})
