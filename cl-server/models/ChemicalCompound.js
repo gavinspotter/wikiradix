802c1ecd7869e99cd95bd9f86b29e7269aa51474
Osiris <potterosiris@gmail.com>
2021-03-08 18:57:13 -0500

    chemical compound schema

--- cl-server/models/ChemicalCompound.js
+++ cl-server/models/ChemicalCompound.js
@@ -2,3 +2,64 @@ const mongoose = require("mongoose")
 
 
 const Schema = mongoose.Schema
+
+const chemicalCompoundSchema = Schema({
+    name: { type: String, required: true },
+    condensedFormula: { type: String },
+    description: { type: String },
+    lewisStructure: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+    skeletalFormula: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+    perspectiveDrawings: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+
+    conformation: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+
+    haworth: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+
+    fischer: [{
+        renditions: [{
+            imageString: { type: String },
+            date: { type: Date }
+        }],
+        currentImage: { type: String },
+
+    }],
+
+
+
+
+})
