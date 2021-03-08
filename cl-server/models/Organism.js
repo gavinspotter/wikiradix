7b2444a7e32b817cbae3d77eb49407e2b540976b
Osiris <potterosiris@gmail.com>
2021-03-07 20:18:26 -0500

    organism schema

--- cl-server/models/Organism.js
+++ cl-server/models/Organism.js
@@ -1,4 +1,11 @@
 const mongoose = require("mongoose")
 
-
 const Schema = mongoose.Schema
+
+const organismSchema = Schema({
+    name: { type: String, required: true },
+    scientificName: { type: String, required: true },
+    Systems: [{}],
+
+})
+
