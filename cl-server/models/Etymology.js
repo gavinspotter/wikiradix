9cbd3ba58db375b279f37af8756529d0686b8e4e
Osiris <potterosiris@gmail.com>
2021-03-10 15:36:27 -0500

    export

--- cl-server/models/Etymology.js
+++ cl-server/models/Etymology.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema
 const etymologySchema = Schema({
     root: { type: String, required: true },
     definition: { type: String, required: true },
-    language: { type: String }
-
+    language: { type: String },
+    words: [{ type: mongoose.Types.ObjectId, ref: "Word" }]
 })
 
 module.exports = mongoose.model("Etymology", etymologySchema)
--- cl-server/models/Word.js
+++ cl-server/models/Word.js
@@ -1,3 +1,13 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const wordSchema = Schema({
+    word: { type: String, required: true },
+    definition: [{
+        type: String, required: true
+    }],
+    etymology: [{ type: mongoose.Types.ObjectId, ref: "Etymology" }]
+})
+
+module.exports = mongoose.model("Word", wordSchema)
