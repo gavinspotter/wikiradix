b2e6a396f62d2783654363596f4653ca9d037b95
Osiris <potterosiris@gmail.com>
2021-03-10 14:21:39 -0500

    etymology

--- cl-server/models/Etymology.js
+++ cl-server/models/Etymology.js
@@ -1,3 +1,10 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const etymologySchema = Schema({
+    root: { type: String, required: true },
+    definition: { type: String, required: true },
+    language: { type: String }
+
+})
