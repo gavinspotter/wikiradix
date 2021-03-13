dc23ee087796190dca38dccff586abe1d6154129
Osiris <potterosiris@gmail.com>
2021-03-13 16:55:28 -0500

    admin schema

--- cl-server/models/Admin.js
+++ cl-server/models/Admin.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const adminSchema = Schema({
+    username: { required: true, type: String },
+    password: { required: true, type: String }
+})
