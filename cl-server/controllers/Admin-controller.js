d7d4fe98063444c1433789e5b097260aa36f74b2
Osiris <potterosiris@gmail.com>
2021-03-13 18:38:30 -0500

    main admin

--- cl-server/controllers/Admin-controller.js
+++ cl-server/controllers/Admin-controller.js
@@ -1,9 +1,15 @@
 const HttpError = require("../models/HttpError")
 
 const Admin = require("../models/Admin")
+const MainAdmin = require("../models/MainAdmin")
 
 
 const requestAdmindPermission = async (req, res, next) => {
+
+
+
+
+
 
 }
 
