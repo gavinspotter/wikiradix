f8b89a7a4c2b55c62023601cc5998667bd4d46ea
Osiris <potterosiris@gmail.com>
2021-03-13 19:36:32 -0500

    mongoose unique validator

--- cl-server/models/Admin.js
+++ cl-server/models/Admin.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const adminSchema = Schema({
     username: { required: true, type: String },
     password: { required: true, type: String },
+    email: { required: true, type: String },
     profilePicture: { type: String, required: true }
 })
 
