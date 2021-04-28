347ff6c551d7e58a4f563c3bfb008e2651025381
Osiris <potterosiris@gmail.com>
2021-04-28 14:44:21 -0400

    controller user

---
+++ cl-server/controllers/User-controller.js
@@ -0,0 +1,10 @@
+const HttpError = require("../models/HttpError")
+
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const login = async (req, res, next) => {
+
+}
+
+exports.login = login
--- cl-server/routes/User-route.js
+++ cl-server/routes/User-route.js
@@ -1,5 +1,8 @@
 const express = require("express")
 
+const userController = require("../controllers/User-controller")
+
+
 
 const router = express.Router()
 
