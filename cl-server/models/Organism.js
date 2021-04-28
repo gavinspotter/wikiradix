f12d8be17de92ece2e5a1e51b6e546363a38e12b
Osiris <potterosiris@gmail.com>
2021-04-28 13:38:19 -0400

    organism schema

--- cl-server/models/Organism.js
+++ cl-server/models/Organism.js
@@ -5,9 +5,10 @@ const Schema = mongoose.Schema
 const organismSchema = Schema({
     name: { type: String, required: true },
     scientificName: { type: String, required: true },
-    Systems: [{ type: mongoose.Types.ObjectId, ref: "System" }],
-    disorders: [{ type: mongoose.Types.ObjectId, ref: "Disorder" }],
-    etymology: [{ type: mongoose.Types.ObjectId, ref: "Etymology" }]
+    Systems: [{ type: mongoose.Types.ObjectId, required: true, ref: "System" }],
+    disorders: [{ type: mongoose.Types.ObjectId, required: true, ref: "Disorder" }],
+    etymology: [{ type: mongoose.Types.ObjectId, required: true, ref: "Etymology" }],
+    species: [{ type: mongoose.Types.ObjectId }]
 })
 
 module.exports = mongoose.model("Organism", organismSchema)
