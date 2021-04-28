a141ee2e9d6527ef01942f4b250a3906c4078c87
Osiris <potterosiris@gmail.com>
2021-04-28 14:15:44 -0400

    phylum schema

--- cl-server/models/Phylum.js
+++ cl-server/models/Phylum.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
-const domainSchema = Schema({
+const phylumSchema = Schema({
     name: { type: String, required: true },
     description: { type: String },
-    kingdoms: [{ type: mongoose.Types.ObjectId, required: true }]
+    class: [{ type: mongoose.Types.ObjectId, required: true, ref: "Class" }],
+    kingdom: { type: mongoose.Types.ObjectId, ref: "Kingdom" }
 })
 
-module.exports = mongoose.model("Domain", domainSchema)
+module.exports = mongoose.model("Phylum", phylumSchema)
