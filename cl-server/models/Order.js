fa9ec82bd63d1d23abf1eda81be7fd0af583bad5
Osiris <potterosiris@gmail.com>
2021-04-28 14:07:28 -0400

    order schema

--- cl-server/models/Order.js
+++ cl-server/models/Order.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
-const domainSchema = Schema({
+const orderSchema = Schema({
     name: { type: String, required: true },
     description: { type: String },
-    kingdoms: [{ type: mongoose.Types.ObjectId, required: true }]
+    family: [{ type: mongoose.Types.ObjectId, required: true, ref: "Family" }],
+    class: { type: mongoose.Types.ObjectId, ref: "Class" }
 })
 
-module.exports = mongoose.model("Domain", domainSchema)
+module.exports = mongoose.model("Order", orderSchema)
